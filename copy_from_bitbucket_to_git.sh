8fa80e7fd5acd5e8ab4fc229e773246a15c1f815
MultipleCrashes <MultipleCrashes@users.noreply.github.com>
2017-06-30 22:04:38 +0530

    Update and rename copy_from_bitbucket_to_git to copy_from_bitbucket_to_git.sh

--- copy_from_bitbucket_to_git.sh
+++ copy_from_bitbucket_to_git.sh
@@ -1,4 +1,5 @@
-cd project_dir
+project_dir="project_dir"
+cd $project_dir
 git remote rename origin bitbucket
 git remote add origin https://github.com/MultipleCrashes/awesome-new-repo.git
 git push origin master

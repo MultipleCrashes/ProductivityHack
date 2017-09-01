b0e2292c950df2f413b503cd555471f2457f1abe
MultipleCrashes <MultipleCrashes@users.noreply.github.com>
2017-09-01 14:44:38 +0530

    Update vim_setup.sh

--- vim_setup.sh
+++ vim_setup.sh
@@ -17,7 +17,7 @@ touch ~/.vimrc
 sudo apt-get install build-essential cmake
 sudo apt-get install python-dev python3-dev
 sudo apt-get install git-core 
-sudo apt-get install python-virtualenv
+sudo apt-get install -y virtualenvwrapper python-virtualenv python-pip
 
 git clone https://github.com/Valloric/YouCompleteMe.git ~/.vim/bundle/
 
